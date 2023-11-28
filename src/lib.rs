521ce38393e9a4cd38790836f500f8e6e0553b47
Adin Ackerman <adinackerman@gmail.com>
2023-11-28 09:52:45 -0800

    update test for tuple-variant enums

--- src/lib.rs
+++ src/lib.rs
@@ -43,9 +43,13 @@ macro_rules! primitive_ser_impls {
 
 primitive_ser_impls! {
     u8: 1,
+    i8: 1,
     u16: 2,
+    i16: 2,
     u32: 4,
+    i32: 4,
     u64: 8,
+    i64: 8,
     f32: 4,
     f64: 8
 }
--- src/tests.rs
+++ src/tests.rs
@@ -6,15 +6,28 @@ use tiny_serde_macros::{Deserialize, Serialize};
 fn proc_macros() {
     #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
     struct Foo {
-        a: u8,
+        a: bool,
         b: u16,
     }
+
+    const NUM: u16 = 0xff;
 
     #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
     #[repr(u16)]
     enum Eenie {
         A = 0xde,
         B,
+        C,
+        D = NUM,
+        E,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+    #[repr(u8)]
+    enum Meenie {
+        A,
+        B(bool) = 0x10,
+        C(u16),
     }
 
     #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -22,17 +35,24 @@ fn proc_macros() {
         something: u16,
         foo: Foo,
         other: Eenie,
+        another: Meenie,
+        lastly: Meenie,
     }
 
     let bar = Bar {
         something: 0x10,
-        foo: Foo { a: 0x2c, b: 0x100 },
+        foo: Foo { a: false, b: 0x100 },
         other: Eenie::B,
+        another: Meenie::C(300),
+        lastly: Meenie::A,
     };
 
     let buf = bar.clone().serialize();
 
-    assert_eq!(buf, [0x0, 0x10, 0x2c, 0x1, 0x0, 0x00, 0xdf]);
+    assert_eq!(
+        buf,
+        [0x0, 0x10, 0x0, 0x1, 0x0, 0x00, 0xdf, 0x11, 0x1, 0x2c, 0x0, 0x0, 0x0]
+    );
 
     if let Some(debar) = Bar::deserialize(buf) {
         assert_eq!(bar, debar);
